2b66176871c57ec05e35eb2c8c9f9acc594b2b6e
yasserveleda <veledinha-a@hotmail.com>
2019-09-08 14:33:09 -0300

    Adicionando rotas e componente home

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'trabalh-conclusao'`, () => {
+  it(`should have as title 'trabalho-conclusao'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('trabalh-conclusao');
+    expect(app.title).toEqual('trabalho-conclusao');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to trabalh-conclusao!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to trabalho-conclusao!');
   });
 });
